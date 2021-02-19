c129d0f445e2173481330bb35988edf45cb48f9a
stevenheroku <herokujeff@gmail.com>
2021-02-18 23:59:01 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String NumTarjeta = "1245874512536589";
+            //int tam_var = NumTarjeta.Length;
+            //String Var_Sub = NumTarjeta.Substring(tam_var - 4);
+
+
+            //Console.Write(Var_Sub);
+
+            string ClsCommon = "3456123434561234";
+
+            if (ClsCommon.Length == 16)
+            {
+                int tam = ClsCommon.Length;
+              string var=  string.Concat("XXXXXXXXXXXX",ClsCommon.Substring(tam-15));
+                Console.Write(var);
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
